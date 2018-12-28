f23238fc1a26a57196d7516729360c7335cd151d
bramz <brockramz@gmail.com>
2018-12-28 02:53:28 -0500

    Command handling

--- apophis/core/handlers.py
+++ apophis/core/handlers.py
@@ -1,8 +1,18 @@
 import cmds
+from typing import List
 
-triggers = ['test']
+class CmdHandler:
+   def __init__(self, bot):
+      self.bot = bot
+      self.cmds = ['!help', '!test']
 
-async def parse_commands(message):
-   if message.content.startswith('!'):
-      for trigger in triggers:
-         cmds.trigger.init(message)
+   def add_cmd(self, cmd):
+      self.cmds.append(cmd)
+   
+   def get_cmds(self) -> List:
+      return self.cmds
+
+   def parse_cmds(self, message) -> str:
+      for cmd in self.cmds:
+         if message.content.startswith(cmd):
+            return self.bot.send_message(message.channel)
