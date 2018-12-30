f0183fc56adba129eefc6e57773921b2d054c0c6
bramz <brockramz@gmail.com>
2018-12-30 03:28:37 -0500

    Update to pass linter

--- apophis/cmds/music.py
+++ apophis/cmds/music.py
@@ -1,17 +1,17 @@
 from cmds.command import Command
 
+
 class MusicCommand(Command):
     def __init__(self, cmd_data):
         self.cmd_data = cmd_data
 
     def parse_command(self, message):
+        usage = "usage: #music playlist <add/del/play>"
         content = message.content[7:].split(
             " ",
             message.content[7:].count(" ")
         )
 
-        usage = "usage: #music playlist <add/del/play>"
-    
         if len(content) == 2:
             if content[0].startswith('playlist'):
                 if content[1].startswith('add'):
