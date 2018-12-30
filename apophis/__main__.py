bad2e96073157310188d7e63dbdc70e3b3941a04
bramz <brockramz@gmail.com>
2018-12-30 01:57:43 -0500

    Fix argument amount

--- apophis/__main__.py
+++ apophis/__main__.py
@@ -3,9 +3,9 @@ import core.client
 
 
 def start_cli():
-    if len(sys.argv) == 3:
+    if len(sys.argv) == 2:
         cli = core.client.BotClient()
-        cli.run(sys.argv[2])
+        cli.run(sys.argv[1])
     else:
         print("To start bot a token is required.")
 
