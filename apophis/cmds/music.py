2ccaabea1533f1fdad3bbb2a25965cd53b569f24
bramz <brockramz@gmail.com>
2018-12-30 06:01:37 -0500

    Update music command

--- apophis/cmds/music.py
+++ apophis/cmds/music.py
@@ -1,4 +1,41 @@
+import json
 from cmds.command import Command
+from typing import NamedTuple
+
+
+class PlaylistData(NamedTuple):
+    url: str
+
+
+def read_playlist_file(filename: str) -> PlaylistData:
+    try:
+        with open('data/music/' + filename + '.json', 'r') as fh:
+            data = json.load(fh)
+            return PlaylistData(data['url'])
+    except IOError:
+        return PlaylistData('')
+
+
+def write_playlist_file(filename: str, playlist_data: PlaylistData) -> None:
+    file_data = {
+        'url': playlist_data,
+    }
+
+    try:
+        with open('data/music/' + filename + '.json', 'a+') as fh:
+            fh.write(json.dumps(file_data))
+    except IOError:
+        with open('data/music/' + filename + '.json', 'a+') as fh:
+            fh.write(json.dumps(file_data))
+
+
+def playlist_file_exists(filename: str) -> bool:
+    try:
+        fh = open('data/music/' + filename + '.json', 'r')
+        fh.close()
+        return True
+    except IOError:
+        return False
 
 
 class MusicCommand(Command):
@@ -12,16 +49,36 @@ class MusicCommand(Command):
             message.content[7:].count(" ")
         )
 
-        if len(content) == 2:
+        if len(content) >= 2:
             if content[0].startswith('playlist'):
                 if content[1].startswith('add'):
-                    return message.channel.send(content)
+                    if playlist_file_exists(content[2]):
+                        write_playlist_file(content[2], content[3])
+                        return message.channel.send(
+                            'Adding ``{0}`` to playlist ``{1}``'
+                            .format(content[3], content[2])
+                        )
+                    else:
+                        write_playlist_file(content[2], content[3])
+                        return message.channel.send(
+                            'Creating new playlist ``{0}`` and adding ``{1}`` to the list.'
+                            .format(content[2], content[3])
+                        )
                 elif content[1].startswith('del'):
-                    return message.channel.send(content)
+                    return message.channel.send(
+                        'Removing ``{0}`` from playlist ``{1}``'
+                        .format(content[3], content[2])
+                    )
                 elif content[1].startswith('play'):
-                    return message.channel.send(content)
+                    return message.channel.send(
+                        '!play {0}'.format(content[2])
+                    )
                 else:
                     return message.channel.send(usage)
+            elif content[0].startswith('play'):
+                return message.channel.send(
+                    '!play {0}'.format(content[1])
+                )
             else:
                 return message.channel.send(usage)
         else:
