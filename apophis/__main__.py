d532c1f1fc8f95a5e95fe8da99927d4ebe5dd766
bramz <brockramz@gmail.com>
2019-01-21 08:58:27 -0500

    Add permissions

--- apophis/__main__.py
+++ apophis/__main__.py
@@ -40,15 +40,16 @@ async def on_message(message):
         return
     else:
         prefix = config['prefix']
+        commands = cmds.command.commands
         for n in range(len(prefix)):
             if message.content.startswith(prefix[n]):
-                for i in range(len(cmds.command.commands)):
+                for i in range(len(commands)):
                     if message.content[1:].startswith(
-                            cmds.command.commands[i]['trigger']
+                            commands[i]['trigger']
                     ):
                         c = cmds.command.command_handler(
-                            cmds.command.commands[i]['module'],
-                            cmds.command.commands[i]['handler']
+                            commands[i]['module'],
+                            commands[i]['handler']
                         )
 
                         async with pool.acquire() as connection:
@@ -58,11 +59,29 @@ async def on_message(message):
                                 'db': connection
                             }
 
-                            await c.handle(
-                                cmds.command.commands[i],
-                                context,
-                                message,
-                            )
+                            if commands[i]['permissions'] == 0:
+                                await c.handle(
+                                    commands[i],
+                                    context,
+                                    message,
+                                )
+                            else:
+                                perms = await cmds.command.check_permissions(
+                                    context, message.author.id
+                                )
+                                if perms is True:
+                                    await c.handle(
+                                        commands[i],
+                                        context,
+                                        message,
+                                    )
+                                else:
+                                    return await message.channel.send(
+                                        '''
+                                        You do not have access
+                                        to utilize that command.
+                                        '''
+                                    )
 
 
 if __name__ == "__main__":
--- apophis/cmds/command.py
+++ apophis/cmds/command.py
@@ -12,38 +12,47 @@ def command_handler(module, handler) -> Command:
     return cmd
 
 
-async def check_permissions(user, context) -> int:
+async def check_permissions(context, user) -> bool:
     statement = '''
     select level from permissions where username = $1;
     '''
-    rows = await context['db'].fetch(statement, user)
-    return int(rows[0]['level'])
+    rows = await context['db'].fetch(statement, str(user))
+
+    if rows[0]['level'] >= 1:
+        return True
+    else:
+        return False
 
 
 commands = [
     {
         'trigger': 'theo',
         'module': 'cmds.theo',
-        'handler': 'TheoCommand'
+        'handler': 'TheoCommand',
+        'permissions': 0
     },
     {
         'trigger': 'help',
         'module': 'cmds.help',
-        'handler': 'HelpCommand'
+        'handler': 'HelpCommand',
+        'permissions': 0
     },
     {
         'trigger': 'define',
         'module': 'cmds.define',
-        'handler': 'DefineCommand'
+        'handler': 'DefineCommand',
+        'permissions': 0
     },
     {
         'trigger': 'music',
         'module': 'cmds.music',
-        'handler': 'MusicCommand'
+        'handler': 'MusicCommand',
+        'permissions': 1
     },
     {
         'trigger': 'access',
         'module': 'cmds.access',
-        'handler': 'AccessCommand'
+        'handler': 'AccessCommand',
+        'permissions': 2
     }
 ]
