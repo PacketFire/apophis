ff435bc9a07e976822992388f4939bf9fdb3b28c
sabinprj <sabin.parajuli13@gmail.com>
2019-01-22 22:07:37 +0545

    fix: restricted fetching of definitions beyond limit and using negative integers

--- apophis/cmds/define.py
+++ apophis/cmds/define.py
@@ -4,24 +4,29 @@ from cmds.command import Command
 
 class DefineCommand(Command):
     def handle(self, context, message):
-        content = list(message.content[8:].split())
-        last = content[-1]
+        content = message.content[8:].split()
+
+        try:
+            index = int(content[-1])
+            term = ' '.join(content[:-1])
+        except ValueError:
+            index = 1
+            term = ' '.join(content)
+
         r = requests.get(
             "http://api.urbandictionary.com/v0/define?term={}"
-            .format(content)
+            .format(term)
         )
         data = r.json()
+        definitions = data.get("list", [])
+        total = len(definitions) if definitions else 0
 
-        if last.isdigit():
-            return message.channel.send(
-                message.content[8:-1] +
-                "[" + str(last) + "/" +
-                str(len(data['list'])-1) +
-                "]: " + data['list'][int(last)]['definition']
-            )
+        if total == 0:
+            reply = "{0} is not defined yet".format(term)
+        elif index > total or index < 1:
+            reply = "cannot find defintion on index {0}".format(index)
         else:
-            return message.channel.send(
-                message.content[8:] +
-                "[0/" + str(len(data['list'])-1) + "]: " +
-                data['list'][0]['definition']
-            )
+            definition = definitions[index-1].get("definition")
+            reply = f"{term} [{index}/{total}]: {definition}"
+
+        return message.channel.send(reply)
