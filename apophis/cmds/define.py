37971f4d8dafac05b1838710c707cf0e5a3e501a
sabinprj <sabin.parajuli13@gmail.com>
2019-01-25 13:41:50 +0545

    fix: fixed an issue where definitions for numbers couldn't be fetched

--- apophis/cmds/define.py
+++ apophis/cmds/define.py
@@ -6,12 +6,16 @@ class DefineCommand(Command):
     def handle(self, context, message):
         content = message.content[8:].split()
 
-        try:
-            index = int(content[-1])
-            term = str.join(' ', content[:-1])
-        except ValueError:
+        if len(content) == 1:
             index = 1
             term = str.join(' ', content)
+        else:
+            try:
+                index = int(content[-1])
+                term = str.join(' ', content[:-1])
+            except ValueError:
+                index = 1
+                term = str.join(' ', content)
 
         r = requests.get(
             f"http://api.urbandictionary.com/v0/define?term={term}"
@@ -23,7 +27,7 @@ class DefineCommand(Command):
         if total == 0:
             reply = f"{term} is not defined yet"
         elif index > total or index < 1:
-            reply = f"cannot find defintion on index {index}"
+            reply = f"cannot find definition on index {index}"
         else:
             definition = definitions[index-1].get("definition")
             reply = f"{term} [{index}/{total}]: {definition}"
