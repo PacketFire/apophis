ebb831c0a70b069271d2d8c68aded28be9b5295c
bramz <brockramz@gmail.com>
2019-01-31 17:29:23 -0500

    Add zip and place sub commands

--- apophis/cmds/weather.py
+++ apophis/cmds/weather.py
@@ -8,21 +8,46 @@ class WeatherCommand(Command):
 
         if content[0] is not None:
             owm = pyowm.OWM(context['config']['weather_token'])
-            observe = owm.weather_at_place(message.content[9:])
-            weather = observe.get_weather()
+            if content[0] == 'zip':
+                if len(content) == 3:
+                    country = content[2]
+                else:
+                    country = 'US'
 
-            return await message.channel.send(
-                'The current weather for {0}: '
-                ':thermometer: temperature {1}˚F, :droplet: humidity {2}, '
-                ':wind_blowing_face: wind speed {3} mph.'
-                .format(
-                    message.content[9:],
-                    weather.get_temperature('fahrenheit')['temp'],
-                    weather.get_humidity(),
-                    weather.get_wind()['speed']
+                observe = owm.weather_at_zip_code(content[1], country)
+                weather = observe.get_weather()
+
+                return await message.channel.send(
+                    'The current weather for {0}: '
+                    ':thermometer: temperature {1}˚F, :droplet: humidity {2}, '
+                    ':wind_blowing_face: wind speed {3} mph.'
+                    .format(
+                        message.content[13:],
+                        weather.get_temperature('fahrenheit')['temp'],
+                        weather.get_humidity(),
+                        weather.get_wind()['speed']
+                    )
                 )
-            )
+            elif content[0] == 'place':
+                observe = owm.weather_at_place(message.content[15:])
+                weather = observe.get_weather()
+
+                return await message.channel.send(
+                    'The current weather for {0}: '
+                    ':thermometer: temperature {1}˚F, :droplet: humidity {2}, '
+                    ':wind_blowing_face: wind speed {3} mph.'
+                    .format(
+                        message.content[13:],
+                        weather.get_temperature('fahrenheit')['temp'],
+                        weather.get_humidity(),
+                        weather.get_wind()['speed']
+                    )
+                )
+
         else:
             return await message.channel.send(
-                'usage: !weather <City,State,Country>'
+                '''
+                usage: !weather <zip/place>
+                zip: <code> <country> place: <City,State,Country>
+                '''
             )
