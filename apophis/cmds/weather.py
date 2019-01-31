f1414f141d579e85e2830f88ad03688938d14750
Brock Ramsey <brockramz@gmail.com>
2019-01-31 16:37:49 -0500

    Weather command (#86)
    * Add weather command closes #84
    
    * Add return statement
    
    * Add missing else branch

---
+++ apophis/cmds/weather.py
@@ -0,0 +1,28 @@
+from cmds.command import Command
+import pyowm
+
+
+class WeatherCommand(Command):
+    async def handle(self, context, message) -> str:
+        content = message.content[8:].split()
+
+        if content[0] is not None:
+            owm = pyowm.OWM(context['config']['weather_token'])
+            observe = owm.weather_at_place(content[0])
+            weather = observe.get_weather()
+
+            return await message.channel.send(
+                'The current weather for {0}: '
+                ':thermometer: temperature {1}˚F, :droplet: humidity {2}, '
+                ':wind_blowing_face: wind speed {3} mph.'
+                .format(
+                    content[0],
+                    weather.get_temperature('fahrenheit')['temp'],
+                    weather.get_humidity(),
+                    weather.get_wind()['speed']
+                )
+            )
+        else:
+            return await message.channel.send(
+                'usage: !weather <zipcode>'
+            )
