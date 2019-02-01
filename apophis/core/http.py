7080360b969328f6a37c58a76624f3edef186c32
Brock Ramsey <brockramz@gmail.com>
2019-02-01 00:18:11 -0500

    Aiohttp (#83)
    * Setup aiohttpd to run concurrently with bot
    
    * use apprunner
    
    * lint

---
+++ apophis/core/http.py
@@ -0,0 +1,20 @@
+from aiohttp import web
+
+
+async def index(request):
+    return web.Response(text="Apophis Web")
+
+
+async def http_handler():
+    app = web.Application()
+    app.add_routes([web.get('/', index)])
+
+    runner = web.AppRunner(app)
+    await runner.setup()
+
+    site = web.TCPSite(runner, '127.0.0.1', 5002)
+    await site.start()
+
+    print('Serving on http://127.0.0.1:5002/')
+
+    # TODO: Handle finish signal here
